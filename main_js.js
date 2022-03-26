8aa8d7ac3a3034a6971cec0daaa085895f2e916f
jeli01 <61683188+jeli01@users.noreply.github.com>
2022-03-26 19:47:25 +0900

    modal window complete

--- main_js.js
+++ main_js.js
@@ -5,6 +5,7 @@ $modalContainer2 = document.querySelector('.modal-container#modal-comment-contai
 $heartButton = document.querySelector('#heart-button');
 $commentButton = document.querySelector('#comment-button');
 $heartImage = document.querySelector('#heart-image');
+$cancleButton = document.querySelector('#cancle-button');
 let image_flag = 0;                                                               // dom 객체 받아오기
 
 $pbMainHeaderList.addEventListener('click', () => {                               // 클릭하면 모달창 나오게
@@ -17,6 +18,10 @@ $commentButton.addEventListener('click', () => {
 
 $listModalBoxInnerFinalBoxButton.addEventListener('click', () => {                // 취소 클릭하면 모달창 꺼지게
   $modalContainer1.style.display = "none";
+})
+
+$cancleButton.addEventListener('click', () => {                // 취소 클릭하면 모달창 꺼지게
+  $modalContainer2.style.display = "none";
 })
 
 $modalContainer1.addEventListener('click', (event) => {                      // 모달창 바깥쪽 클릭하면 꺼지게 구현!!
