0c4d0797a60c2165ed4093ea883f11342f4d19a3
jeli01 <61683188+jeli01@users.noreply.github.com>
2022-03-26 17:43:17 +0900

    modal window 구현중

--- main_js.js
+++ main_js.js
@@ -1,21 +1,27 @@
 $pbMainHeaderList = document.querySelector(".pb-main-header-list");
 $listModalBoxInnerFinalBoxButton = document.querySelector('#list-modal-box-inner-final-box button');
-$modalContainer = document.querySelector('.modal-container');
+$modalContainer1 = document.querySelector('.modal-container#modal-list-container');
+$modalContainer2 = document.querySelector('.modal-container#modal-comment-container');
 $heartButton = document.querySelector('#heart-button');
+$commentButton = document.querySelector('#comment-button');
 $heartImage = document.querySelector('#heart-image');
 let image_flag = 0;                                                               // dom 객체 받아오기
 
 $pbMainHeaderList.addEventListener('click', () => {                               // 클릭하면 모달창 나오게
-  $modalContainer.style.display = "flex";
+  $modalContainer1.style.display = "flex";
+});
+
+$commentButton.addEventListener('click', () => {                    
+  $modalContainer2.style.display = "flex";
 });
 
 $listModalBoxInnerFinalBoxButton.addEventListener('click', () => {                // 취소 클릭하면 모달창 꺼지게
-  $modalContainer.style.display = "none";
+  $modalContainer1.style.display = "none";
 })
 
-$modalContainer.addEventListener('click', (event) => {                      // 모달창 바깥쪽 클릭하면 꺼지게 구현!!
+$modalContainer1.addEventListener('click', (event) => {                      // 모달창 바깥쪽 클릭하면 꺼지게 구현!!
   if(event.target == event.currentTarget)
-    $modalContainer.style.display = "none";
+    $modalContainer1.style.display = "none";
 });
 
 $heartButton.addEventListener('click', () => {                            // 하트 클릭 색깔 변하기 구현!!
